c2905bc4957774e24705b9f7b8692b5db6a2bf23
cconst <4udconst@gmail.com>
2015-01-12 01:23:34 +0600

    some commit
    don't know what is the shit happens here

---
+++ src/main/java/ru/nstu/cs/cconst/Application.java
@@ -0,0 +1,35 @@
+package ru.nstu.cs.cconst;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("ru.nstu.cs.cconst")
+@EntityScan("ru.nstu.cs.cconst.model")
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		configureApplication(new SpringApplicationBuilder()).run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
+
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder
+			.showBanner(false)
+			.sources(Application.class)
+			.web(true);
+	}
+}
