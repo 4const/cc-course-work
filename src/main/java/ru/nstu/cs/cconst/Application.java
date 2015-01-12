2649d3b7ae1c2aa082bc96020ffd1ca784da6e05
Constantin Chudimov <cchudimov@eldis.ru>
2015-01-12 10:14:04 +0600

    setup spring boot

--- src/main/java/ru/nstu/cs/cconst/Application.java
+++ src/main/java/ru/nstu/cs/cconst/Application.java
@@ -1,24 +1,22 @@
 package ru.nstu.cs.cconst;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan("ru.nstu.cs.cconst")
+@SpringBootApplication
 @EntityScan("ru.nstu.cs.cconst.model")
+@ImportResource({"classpath*:beans.xml"})
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		configureApplication(new SpringApplicationBuilder()).run(args);
+        configureApplication(new SpringApplicationBuilder()).run(args);
 	}
 
 	@Override
@@ -27,9 +25,8 @@ public class Application extends SpringBootServletInitializer {
 	}
 
 	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-		return builder
+        return builder
 			.showBanner(false)
-			.sources(Application.class)
-			.web(true);
+			.sources(Application.class);
 	}
 }
